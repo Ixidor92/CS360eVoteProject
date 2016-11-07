33f5313737a6f0694f84bff274322bf3ae0a96b8
montyms <montyms@g.cofc.edu>
2016-11-07 10:49:34 -0500

    Update Election.java

--- Election.java
+++ Election.java
@@ -1,13 +1,24 @@
 package eVoting;
 
 import java.util.ArrayList;
-inport java.util.Scanner;
+import java.util.Scanner;
 
 public class Election 
 {
 	private ArrayList<Nominee> nominees;	//a list of available nominees
 	private boolean currentlyRunning;		//is the election currently active?
 	private int voteTally;					//tally of those who have voted
+	
+	public static void main(String [] args)
+	{
+		System.out.println("Enter a Username:\t");
+		String username = scanner.nextLine();
+		
+		if ( 
+		
+		
+		
+	}
 	
 	
 }
